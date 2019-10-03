e94642eb5c5ed3fe48cbe2fd21821f7ad6d941a8
Stefaan <safwebdev@gmail.com>
2019-10-03 15:46:14 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form';
+import Weather from './components/Weather';
+import Navbar from './components/Navbar';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_KEY = process.env.REACT_APP_KEY;
+
+class App extends React.Component {
+  
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    temp_min: undefined,
+    temp_max: undefined,
+    humidity: undefined,
+    description: undefined,
+    icon: undefined,
+    error: undefined,
+    location: undefined,
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const location = e.target.value;
+    console.log(location);
+    const units = "metric";
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    if (location) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        temp_min: data.main.temp_min,
+        temp_max: data.main.temp_max,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        icon: data.weather[0].icon,
+        error: ""
+      })
+    }
+    else {
+      this.setState({
+        temperature: undefined,
+        temp_min: undefined,
+        temp_max: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please Enter values"
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container section">
+          <div className="row weather">
+            <div className="col xl8 l8 m6 s12 output">
+              <Weather
+                temperature={this.state.temperature}
+                temp_min={this.state.temp_min}
+                temp_max={this.state.temp_max}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                icon={this.state.icon}
+                error={this.state.error} />
+            </div>
+            <div className="col xl4 l4 m6 s12 form">
+              <Form getWeather={this.getWeather} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Weather = props => (
+    <div>
+        
+        { !props.city && !props.country && 
+            <div className="col s12 title-icon">
+                <h4>Select a Location</h4>
+            </div>
+        }
+        { props.city && props.country && props.icon && props.description && 
+            <div className="col s12 title-icon">
+                <h4>
+                <img src={'http://openweathermap.org/img/wn/'+props.icon+'.png'} alt={props.description}/>
+                {props.city}, {props.country}
+                </h4>
+            </div>
+        }
+        { props.temperature && 
+            <div className="col s12">Currently : {props.temperature} &#8451;</div> 
+        }
+        { props.temp_min && 
+            <div className="col s12">Min : {props.temp_min} &#8451;</div> 
+        }
+        { props.temp_max && 
+            <div className="col s12">Max : {props.temp_max} &#8451;</div> 
+        }
+        { props.humidity && 
+            <div className="col s12">Humidity: {props.humidity}</div> 
+        }
+        { props.description && 
+            <div className="col s12">Conditions: {props.description}</div> 
+        }
+        { props.error && 
+            <div className="col s12">{props.error}</div> 
+        }
+    </div>
+)
+
+export default Weather;
