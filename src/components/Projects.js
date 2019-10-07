634529e8e97276c64cd20ac548b2dc7441c0150a
Stefaan <safwebdev@gmail.com>
2019-10-07 15:01:56 +0800

    fix firebase loop on Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,6 @@ class Projects extends React.Component{
     }
 
     render(){
-        console.log(this.state.projects);
 
         const projectList = this.state.projects;
 
@@ -43,7 +42,7 @@ class Projects extends React.Component{
                                 return <div className="project row col xl6 l6 m6 s12" key={ key }>
                                             <h6>{item.project_name}</h6> 
                                             <p className="desc">{item.project_desc}</p>
-                                            {/* {getTools(item.project_tools)} */}
+                                            {getTools(item.project_tools)}
                                             <div className="links">
                                                 { item.demoURL !== null
                                                 ? <span>
@@ -77,66 +76,6 @@ class Projects extends React.Component{
                     </div>
                     // </Slider>
         }
-
-        
-        // const projectList =[
-        //         {
-        //             name:"Packist",
-        //             desc:"Website of the Malaysian-based online travel agency that offers travel packages, tours, transport and more.",
-        //             tools:["PHP", "Code Igniter", "Bootstrap", "JQuery"],
-        //             demoURL:"https://www.packist.com/",
-        //             githubURL: null,
-        //             type:"client"
-        //         },
-        //         {
-        //             name:"Sureplify",
-        //             desc:"Website for the Malaysian Car Insurance Company that can offer free real time quotation comparisons.",
-        //             tools:["React JS", "Vue JS", "REST API", "Bootstrap"],
-        //             demoURL:"https://www.sureplify.com/#/",
-        //             githubURL: null,
-        //             type:"client"
-        //         },
-        //         {
-        //             name:"BitRatez",
-        //             desc:"BitCoin currency rate checker",
-        //             tools:["NextJS", "Bootstrap"],
-        //             demoURL:null,
-        //             githubURL: "https://github.com/safwbdev/BitRatez",
-        //             type:"personal"
-        //         },
-        //         {
-        //             name:"The Weather Man Can!",
-        //             desc:"Weather App that shows the current from different cities using the openWeather API",
-        //             tools:["React JS", "Material"],
-        //             demoURL:"https://zealous-sammet-364d1d.netlify.com/",
-        //             githubURL: "https://github.com/safwbdev/weather-app",
-        //             type:"personal"
-        //         },
-        //         {
-        //             name:"Online portfolio",
-        //             desc:"The current site you're on. Like it? Made it myself.",
-        //             tools:["React JS", "Material"],
-        //             demoURL:null,
-        //             githubURL: null,
-        //             type:"personal"
-        //         },
-        //         {
-        //             name:"Online portfolio(Vue & Firebase)",
-        //             desc:"This version was originally meant to be used here (with CRUD included). But DAMN, is the react version better! (Yes, I am biased)",
-        //             tools:["Vue JS", "Material", "Firebase"],
-        //             demoURL:null,
-        //             githubURL: "null",
-        //             type:"personal"
-        //         },
-        //         {
-        //             name:"Keto Recipes",
-        //             desc:"Built this as an excercise first, then continued on it to help me lose weight. (Lost 15 kilos thanks to this!)",
-        //             tools:["React JS", "REST API", "Bootstrap"],
-        //             demoURL:null,
-        //             githubURL: null,
-        //             type:"personal"
-        //         },
-        // ];
 
         var settings = {
             dots: true,
