983d596c97ab8e951527959a15933e1fd5aa76f8
Stefaan <safwebdev@gmail.com>
2019-10-11 10:09:59 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Education from './components/Education';
 // import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -26,6 +27,7 @@ class App extends React.Component {
             <Skills  />
             <Experience  />
             <Education  />
+            <Footer />
       </div>
     );
   }
