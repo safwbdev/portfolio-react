9dffff126013fa507d4c6fec85b94e25f706b177
Stefaan <safwebdev@gmail.com>
2019-10-15 10:31:55 +0800

    add loading > add animation to profile > move db code to app file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Projects from './components/ProjectSection/ProjectIndex';
 import Education from './components/EducationSection/EducationIndex';
 import Footer from './components/Footer';
 
+import { db } from "./firebase";
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -17,10 +19,51 @@ library.add(fab,fas,far)
 
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+        basicSkills:[],
+        loading:true
+    }
+}
+componentDidMount() {
+    db.collection("personal")
+        .get()
+        .then(querySnapshot => {
+            const data = querySnapshot.docs.map(doc => doc.data());
+                this.setState({
+                    fName:data[0].fullName,
+                    designation: data[0].designation,
+                    tel: data[0].tel,
+                    email: data[0].email,
+                    address: data[0].address,
+                    github: data[0].githubUrl,
+                    linkedin: data[0].linkedinUrl,
+                    image: data[0].image,
+                    desc: data[0].desc,
+                    loading:false
+                })
+        });
+}
   render() {
-    return (
+    // return (
+      return this.state.image ==null ? (
+        <div className="loadScreen">
+            <p>Loading...</p>
+        </div>
+    ) : (
       <div>
-            <Intro />
+            <Intro
+            fName={this.state.fName}
+            designation={this.state.designation}
+            tel={this.state.tel}
+            email={this.state.email}
+            address={this.state.address}
+            github={this.state.github}
+            linkedin={this.state.linkedin}
+            image={this.state.image}
+            desc={this.state.desc}
+             />
             <Projects  />
             <Skills  />
             <Experience  />
--- src/components/IntroSection/IntroIndex.js
+++ src/components/IntroSection/IntroIndex.js
@@ -1,57 +1,32 @@
 import React from 'react'
-import { db } from "../../firebase";
 import Description from './Description';
 import Profile from './Profile';
+import WOW from "wowjs";
+
 
 class IntroIndex extends React.Component{
 
-    constructor(){
-        super();
-        this.state = {
-            fullName: null,
-            address: null,
-            dob: null,
-            email: null,
-            tel: null,
-            github:null,
-            linkedin:null,
-            image:null,
-            desc: null,
-        }
-    }
     componentDidMount(){
-        db.collection("personal")
-        .get()
-        .then(querySnapshot => {
-            const data = querySnapshot.docs.map(doc => doc.data());
-                this.setState({
-                    fName:data[0].fullName,
-                    designation: data[0].designation,
-                    tel: data[0].tel,
-                    email: data[0].email,
-                    address: data[0].address,
-                    github: data[0].githubUrl,
-                    linkedin: data[0].linkedinUrl,
-                    image: data[0].image,
-                    desc: data[0].desc,
-                })
-        });
+        const wow = new WOW.WOW();
+        wow.init();
     }
 
     render() {
         return (
             <div className="row">
                 <div className="col xl12 l12 m12 s12 intro-section">
-                    <Profile 
-                        image={this.state.image}
-                        fName={this.state.fName}
-                        designation={this.state.designation}
-                        tel={this.state.tel}
-                        email={this.state.email}
-                        github={this.state.github}
-                        linkedin={this.state.linkedin}
-                        address={this.state.address} />
-                    <Description desc={this.state.desc} />
+                    <div className="wow fade">
+                        <Profile 
+                            image={this.props.image}
+                            fName={this.props.fName}
+                            designation={this.props.designation}
+                            tel={this.props.tel}
+                            email={this.props.email}
+                            github={this.props.github}
+                            linkedin={this.props.linkedin}
+                            address={this.props.address} />
+                        <Description desc={this.props.desc} />
+                    </div> 
                 </div>
             </div>
         )
